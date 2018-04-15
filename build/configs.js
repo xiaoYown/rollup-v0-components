0d69e80868db5c21246f0ee76457117b57d41cbe
DESKTOP-9MT21FL\xiaoYown <qzy09101018@sina.com>
2018-04-15 23:00:34 +0800

    Mod_可基本打包

--- build/configs.js
+++ build/configs.js
@@ -1,68 +1,91 @@
-const path = require('path')
-const buble = require('rollup-plugin-buble')
-const flow = require('rollup-plugin-flow-no-whitespace')
-const cjs = require('rollup-plugin-commonjs')
-const node = require('rollup-plugin-node-resolve')
-const replace = require('rollup-plugin-replace')
-const version = process.env.VERSION || require('../package.json').version
+const path = require('path');
+const buble = require('rollup-plugin-buble');
+const flow = require('rollup-plugin-flow-no-whitespace');
+const cjs = require('rollup-plugin-commonjs');
+const node = require('rollup-plugin-node-resolve');
+const replace = require('rollup-plugin-replace');
+const version = process.env.VERSION || require('../package.json').version;
 const banner =
 `/*!
   * piece-plugins v${version}
   * (c) ${new Date().getFullYear()} xiaoYown
   * @license MIT
-  */`
+  */`;
 
-const resolve = _path => path.resolve(__dirname, '../', _path)
+const resolve = _path => path.resolve(__dirname, '../', _path);
 
 module.exports = [
-  // browser dev
   {
-    file: resolve('dist/vue-router.js'),
-    format: 'umd',
-    env: 'development'
-  },
-  {
-    file: resolve('dist/vue-router.min.js'),
-    format: 'umd',
-    env: 'production'
-  },
-  {
-    file: resolve('dist/vue-router.common.js'),
-    format: 'cjs'
-  },
-  {
-    file: resolve('dist/vue-router.esm.js'),
-    format: 'es'
-  }
-].map(genConfig)
-
-function genConfig (opts) {
-  const config = {
     input: {
       input: resolve('src/index.js'),
-      plugins: [
-        flow(),
-        node(),
-        cjs(),
-        replace({
-          __VERSION__: version
-        }),
-        buble()
-      ]
+      // plugins: [
+      //   flow(),
+      //   node(),
+      //   cjs(),
+      //   replace({
+      //     __VERSION__: version
+      //   }),
+      //   buble()
+      // ]
     },
     output: {
-      file: opts.file,
-      format: opts.format,
+      file: resolve('dist/piece.js'),
+      format: 'umd',
       banner,
-      name: 'VueRouter'
+      name: 'piece-pages'
     }
   }
+];
 
-  if (opts.env) {
-    config.input.plugins.unshift(replace({
-      'process.env.NODE_ENV': JSON.stringify(opts.env)
-    }))
-  }
+// module.exports = [
+//   // browser dev
+//   {
+//     file: resolve('dist/vue-router.js'),
+//     format: 'umd',
+//     env: 'development'
+//   },
+//   {
+//     file: resolve('dist/vue-router.min.js'),
+//     format: 'umd',
+//     env: 'production'
+//   },
+//   {
+//     file: resolve('dist/vue-router.common.js'),
+//     format: 'cjs'
+//   },
+//   {
+//     file: resolve('dist/vue-router.esm.js'),
+//     format: 'es'
+//   }
+// ].map(genConfig)
 
-  return config
-}
+// function genConfig (opts) {
+//   const config = {
+//     input: {
+//       input: resolve('src/index.js'),
+//       plugins: [
+//         flow(),
+//         node(),
+//         cjs(),
+//         replace({
+//           __VERSION__: version
+//         }),
+//         buble()
+//       ]
+//     },
+//     output: {
+//       file: opts.file,
+//       format: opts.format,
+//       banner,
+//       name: 'VueRouter'
+//     }
+//   }
+
+//   if (opts.env) {
+//     config.input.plugins.unshift(replace({
+//       'process.env.NODE_ENV': JSON.stringify(opts.env)
+//     }))
+//   }
+
+//   return config
+// }
---
+++ dist/piece.js
@@ -0,0 +1,16 @@
+/*!
+  * piece-plugins v0.0.0
+  * (c) 2018 xiaoYown
+  * @license MIT
+  */
+(function (global, factory) {
+	typeof exports === 'object' && typeof module !== 'undefined' ? factory() :
+	typeof define === 'function' && define.amd ? define(factory) :
+	(factory());
+}(this, (function () { 'use strict';
+
+(function () {
+  console.log('module init');
+})();
+
+})));
